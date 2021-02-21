93d8736ed8061301bbc9384bf40d693dfcac0a18
kawai10 <77530278+kawai10@users.noreply.github.com>
2021-02-21 11:33:48 +0900

    2021-02-21-2

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const title = document.querySelector(#title)
+const age = prompt("How old are you");
 
-function handleclick() {
-    title.style.color = "blue"
+
+if(age >= 18 && age <= 21) {
+    console.log("you can drink but you should not");
+} else if(age > 21) {
+    console.log("go ahead");
+} else {
+    console.log("too young");
 }
-
-title.addventListener("click", handleclick)
